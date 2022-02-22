485fae37ca5a1188d5cda8ce915b5cc4aeb81111
James-Pollock <jamespollock.dev@gmail.com>
2022-02-21 21:08:59 -0600

    add manifest and json data

---
+++ src/components/Loader.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Placeholder } from 'react-bootstrap';
+
+export default function Loader() {
+  return (
+    <>
+    <Placeholder className="Loader"> Loading </Placeholder>
+    </>
+  );
+}
